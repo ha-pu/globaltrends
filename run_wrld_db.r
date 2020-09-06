6eddb46842489b0949aac814fcd0afd4c64a28e4
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-06 23:07:22 +0200

    Make packages explicit

--- run_wrld_db.r
+++ run_wrld_db.r
@@ -3,13 +3,13 @@ run_wrld <- function(object) {
   terms <- terms[!(terms %in% dict_obj$term2)]
   time <- time_obj$time[time_obj$batch == object]
   if (.test_empty(table = "data_wrld", batch_o = object)){
-    out <- map_dfr(terms, ~{
+    out <- purrr::map_dfr(terms, ~{
       out <- .get_trend(geo = "", term = .x, time = time)
-      out <- select(out, -geo)
-      message(str_c("run_wrld | term: ", which(terms == .x), "/", length(terms), " complete [", object, "|", max(terms_obj$batch), "]"))
+      out <- dplyr::select(out, -geo)
+      message(stringr::str_c("run_wrld | term: ", which(terms == .x), "/", length(terms), " complete [", object, "|", max(terms_obj$batch), "]"))
       return(out)
     })  
-    out <- mutate(out, batch = object)
-    dbWriteTable(conn = gtrends_db, name = "data_wrld", value = out, append = TRUE)
+    out <- dplyr::mutate(out, batch = object)
+    DBI::dbWriteTable(conn = gtrends_db, name = "data_wrld", value = out, append = TRUE)
   }
 }
