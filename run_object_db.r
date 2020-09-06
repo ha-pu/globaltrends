801b65910684604078aa4ae3506b77713d0898ee
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-06 22:58:26 +0200

    Make packages explicit

--- run_object_db.r
+++ run_object_db.r
@@ -1,14 +1,16 @@
+# download object data
+
 run_object <- function(object, lst_geo = lst_wdi) {
   terms <- terms_obj$keyword[terms_obj$batch == object]
   time <- time_obj$time[time_obj$batch == object]
-  map(lst_geo, ~{
+  purrr::map(lst_geo, ~{
     if (.test_empty(table = "data_obj", batch_o = object, geo = .x)){
       out <- .get_trend(geo = .x, term = terms, time = time)
       if (!is.null(out)) {
-        out <- mutate(out, batch = object)
-        dbWriteTable(conn = gtrends_db, name = "data_obj", value = out, append = TRUE)
+        out <- dplyr::mutate(out, batch = object)
+        DBI::dbWriteTable(conn = gtrends_db, name = "data_obj", value = out, append = TRUE)
       }
     }
-    message(str_c("run_object | batch: ", object, " | geo: ", .x, " complete [", which(lst_geo == .x), "|", length(lst_geo), "]"))
+    message(stringr::str_c("run_object | batch: ", object, " | geo: ", .x, " complete [", which(lst_geo == .x), "|", length(lst_geo), "]"))
   })
 }
