53ec9ba0512845d5f400acfb3dc084c490739a61
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-11 12:06:33 +0200

    Improve tests

--- test_db.r
+++ test_db.r
@@ -1,49 +1,13 @@
 # run data base test
 
 # packages ----
-library(DBI)
-library(dbplyr)
-library(lubridate)
+library(doiGT)
 library(tidyverse)
 options(dplyr.summarise.inform = FALSE)
 
 # connect to db ----
-gtrends_db_file <- "db/gtrends.sqlite"
-gtrends_db_file_bu <- "db/gtrends_bu.sqlite"
-file.copy(gtrends_db_file, gtrends_db_file_bu)
-gtrends_db <- dbConnect(RSQLite::SQLite(), gtrends_db_file)
-src_dbi(gtrends_db)
-
-# get tables ----
-data_geo <- tbl(gtrends_db, "data_geo")
-batch_terms <- tbl(gtrends_db, "batch_terms")
-batch_time <- tbl(gtrends_db, "batch_time")
-dict_obj <- tbl(gtrends_db, "dict_obj")
-
-data_agg <- tbl(gtrends_db, "data_agg")
-data_con <- tbl(gtrends_db, "data_con")
-data_map <- tbl(gtrends_db, "data_map")
-data_obj <- tbl(gtrends_db, "data_obj")
-data_score <- tbl(gtrends_db, "data_score")
-data_wrld <- tbl(gtrends_db, "data_wrld")
-
-# load files ----
-lst_wdi <- filter(data_geo, type == "lst_wdi" & share >= 0.001)
-lst_wdi <- collect(lst_wdi)
-lst_wdi <- pull(lst_wdi, geo)
-lst_usa <- filter(data_geo, type == "lst_usa")
-lst_usa <- collect(lst_usa)
-lst_usa <- pull(lst_usa, geo)
-
-terms_con <- filter(batch_terms, type == "con")
-terms_con <- collect(terms_con)
-time_con <- filter(batch_time, type == "con")
-time_con <- collect(time_con)
-terms_obj <- filter(batch_terms, type == "obj")
-terms_obj <- collect(terms_obj)
-time_obj  <- filter(batch_time, type == "obj")
-time_obj <- collect(time_obj)
-dict_obj <- collect(dict_obj)
+run_init()
+gtrends_base()
 
 # add new control batch ----
 add_batch(type = "control", keyword = c("gmail", "wikipedia"), time = "2016-01-01 2019-12-31")
@@ -57,22 +21,27 @@ filter(batch_terms, batch == new_batch_o & type == "obj")
 
 # run control download ----
 run_control(control = new_batch_c, lst_geo = lst_wdi[1:5])
+run_control(control = new_batch_c, lst_geo = lst_usa[1:5])
 filter(data_con, batch == new_batch_c)
 
 # run object download ----
 run_object(object = new_batch_o, lst_geo = lst_wdi[1:5])
+run_object(object = new_batch_o, lst_geo = lst_usa[1:5])
 filter(data_obj, batch == new_batch_o)
 
 # run map download ----
-run_map(control = new_batch_c, object = new_batch_o)
+run_map(control = new_batch_c, object = new_batch_o, lst_geo = lst_wdi)
+run_map(control = new_batch_c, object = new_batch_o, lst_geo = lst_usa)
 filter(data_map, batch_c == new_batch_c & batch_o == new_batch_o)
 
 # run scoring ----
-run_score(control = new_batch_c, object = new_batch_o)
+run_score(control = new_batch_c, object = new_batch_o, lst_geo = lst_wdi)
+run_score(control = new_batch_c, object = new_batch_o, lst_geo = lst_usa)
 filter(data_score, batch_c == new_batch_c & batch_o == new_batch_o)
 
 # run aggregation ----
-run_agg(control = new_batch_c, object = new_batch_o)
+run_agg(control = new_batch_c, object = new_batch_o, lst_geo = "lst_wdi")
+run_agg(control = new_batch_c, object = new_batch_o, lst_geo = "lst_usa")
 filter(data_agg, batch_c == new_batch_c & batch_o == new_batch_o)
 
 # run world download ----
@@ -92,5 +61,4 @@ filter(data_score, batch_c == new_batch_c & batch_o == new_batch_o)
 filter(data_wrld, batch == new_batch_o)
 
 # disconnect from db ----
-DBI::dbDisconnect(gtrends_db)
-file.remove(gtrends_db_file_bu)
+disconnect_db()
