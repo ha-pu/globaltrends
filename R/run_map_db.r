b5133476042275e1afcd05397b8303303d38915e
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-12 08:27:50 +0200

    take NULL hits as input

--- R/run_map_db.r
+++ R/run_map_db.r
@@ -41,12 +41,13 @@ run_map <- function(control, object, lst_geo = lst_wdi) {
         term_con <- summarise(group_by(qry_con, keyword), hits = mean(hits))
         term_con <- term_con$keyword[order(term_con$hits)]
         term_obj <- summarise(group_by(qry_obj, keyword), hits = mean(hits))
+        term_obj <- mutate(term_obj, hits = coalesce(hits, 0))
         term_obj <- term_obj$keyword[term_obj$hits == max(term_obj$hits)]
-        date_min <- as_date(max(min(qry_con$date), min(qry_obj$date)))
-        date_max <- as_date(min(max(qry_con$date), max(qry_obj$date)))
+        date_min <- as_date(max(min(qry_con$date), coalesce(min(qry_obj$date), min(qry_con$date))))
+        date_max <- as_date(min(max(qry_con$date), coalesce(max(qry_obj$date), max(qry_con$date))))
         if (date_min < date_max) {
           i <- 1
-          while (i <= 5) {
+          while (i <= length(term_con)) {
             out <- .get_trend(geo = .x, term = c(term_con[[i]], term_obj[[1]]), time = str_c(date_min, date_max, sep = " "))
             if (!is.null(out) & median(out$hits[out$keyword == term_con[[i]]]) > 1) {
               out <- mutate(out, batch_c = control, batch_o = object)
