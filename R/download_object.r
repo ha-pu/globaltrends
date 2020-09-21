0266fcd3d34d60bf327f441cbaf2aeef3cf49c54
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-21 10:55:14 +0200

    Bugfix in download_object

--- R/download_object.r
+++ R/download_object.r
@@ -11,7 +11,7 @@
 #' @param object Object batch for which the data is downloaded. Object
 #' of class \code{numeric} or object of class \code{list} containing single
 #' elements of class \code{numeric}.
-#' @param control Control batch that is used for mapping. Object of class 
+#' @param control Control batch that is used for mapping. Object of class
 #' \code{numeric}. Defaults to \code{1}.
 #' @param locations List of countries or regions for which the data is downloaded.
 #' Refers to lists generated in \code{start_db}.
@@ -46,27 +46,31 @@ download_object.numeric <- function(object, control = 1, locations = countries) 
   .test_batch(object)
   terms_obj <- .keywords_object$keyword[.keywords_object$batch == object]
   time <- .time_object$time[.time_object$batch == object]
-  
-  qry_control <- filter(.tbl_control, batch == control & location == .x)
-  qry_control <- collect(qry_control)
-  if (nrow(qry_control) > 0 ) {
-    terms_con <- summarise(group_by(qry_control, keyword), hits = mean(hits), .groups = "drop")
-    terms_con <- terms_con$keyword[order(terms_con$hits)]
-  }
-  
-  i <- 1
-  while (i <= length(terms_con)) {
-    out <- .get_trend(location = .x, term = c(terms_con[[i]], terms_obj), time = time)
-    if (!is.null(out) & median(out$hits[out$keyword == terms_con[[i]]]) > 1) {
-      out <- mutate(out, batch_c = control, batch_o = object)
-      dbWriteTable(conn = globaltrends_db, name = "data_object", value = out, append = TRUE)
-      break()
+
+  walk(locations, ~ {
+    if (.test_empty(table = "data_object", batch_o = object, batch_c = control, location = .x)) {
+      qry_control <- filter(.tbl_control, batch == control & location == .x)
+      qry_control <- collect(qry_control)
+      if (nrow(qry_control) > 0) {
+        terms_con <- summarise(group_by(qry_control, keyword), hits = mean(hits), .groups = "drop")
+        terms_con <- terms_con$keyword[order(terms_con$hits)]
+      }
+
+      i <- 1
+      while (i <= length(terms_con)) {
+        out <- .get_trend(location = .x, term = c(terms_con[[i]], terms_obj), time = time)
+        if (!is.null(out) & median(out$hits[out$keyword == terms_con[[i]]]) > 1) {
+          out <- mutate(out, batch_c = control, batch_o = object)
+          dbWriteTable(conn = globaltrends_db, name = "data_object", value = out, append = TRUE)
+          break()
+        }
+        i <- i + 1
+      }
+      message(glue("Successfully downloaded object data | object: {object} | 
+                 control: {control} | location: {.x} [{current}/{total}]",
+        current = which(locations == .x), total = length(locations)
+      ))
     }
-    i <- i + 1
-  }
-    message(glue("Successfully downloaded object data | object: {object} | 
-                 control: {control} | location: {.x} [{current}/{total}]", 
-                 current = which(locations == .x), total = length(locations)))
   })
 }
 
