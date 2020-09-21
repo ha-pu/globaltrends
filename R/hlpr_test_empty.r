ced2eaa5f35aad420a3c1800f98c9c0f23f56091
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-21 10:55:03 +0200

    Update hlpr_test_empty.r

--- R/hlpr_test_empty.r
+++ R/hlpr_test_empty.r
@@ -15,9 +15,7 @@
     if (table == "data_control") {
       out <- filter(.tbl_control, batch == in_batch_c & location == in_location)
     } else if (table == "data_object") {
-      out <- filter(.tbl_object, batch == in_batch_o & location == in_location)
-    } else if (table == "data_mapping") {
-      out <- filter(.tbl_mapping, batch_c == in_batch_c & batch_o == in_batch_o & location == in_location)
+      out <- filter(.tbl_object, batch_c == in_batch_c & batch_o == in_batch_o & location == in_location)
     } else if (table == "data_score") {
       out <- filter(.tbl_score, batch_c == in_batch_c & batch_o == in_batch_o & location == in_location)
     } else if (table == "data_doi") {
