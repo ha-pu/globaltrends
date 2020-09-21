7c896ef040b09f04298f081079b80bd16fd51664
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-21 12:16:07 +0200

    Create add_synonyms.r

---
+++ R/add_synonyms.r
@@ -0,0 +1,37 @@
+#' Add keyword synonyms
+#'
+#' @description
+#' @details 
+#'
+#' @param keyword Keyword of type \code{character} for which the synonmys are 
+#' added
+#' @param synonym Synonmy of type \code{character}
+#'
+#' @return
+#' 
+#' @examples
+#' \dontrun{
+#' add_synonym(keyword = "fc bayern", synonym = "bayern munich")
+#' }
+#' 
+#' @export
+#' @importFrom DBI dbWriteTable
+#' @importFrom glue glue
+#' @importFrom tibble tibble
+
+add_synonym <- function(keyword, synonym) {
+  if (length(synonym) > 1) walk(synonym, add_synonym, keyword = keyword)
+  
+  out <- tibble(keyword, synonym)
+  dbWriteTable(
+    conn = globaltrends_db,
+               name = "keyword_synonyms",
+               value = out,
+               append = TRUE
+               )
+  keyword_synonyms <- collect(.tbl_synonyms)
+  lst_export <- list(keyword_synonyms, keyword_synonyms)
+  names(lst_export) <- list("keyword_synonyms", ".keyword_synonyms")
+  invisible(list2env(lst_export, envir = .GlobalEnv))
+  message(glue("Successfully added synonym | keyword: {keyword} | synonym: {synonym}"))
+}
