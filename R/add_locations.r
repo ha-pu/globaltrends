938fdd0c5e7d2495835a4c2aa677856529fc3013
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-12-12 21:28:54 +0100

    Add roxygen

--- R/add_locations.r
+++ R/add_locations.r
@@ -1,18 +1,45 @@
+#' @title Add sets of locations
+#'
+#' @description
+#'
+#' @details
+#'
+#' @param locations xxx.
+#' @param type xxx.
+#' @param export xxx.
+#' @param db Connection to database file that should be closed. Defaults
+#' to \code{globaltrends_db}.
+#'
+#' @return
+#' 
 #' @example
 #' \dontrun{
 #' add_locations(locations = c("AT", "CH", "DE"), type = "DACH")
 #' }
+#' @export
+#' @importFrom DBI dbWriteTable
+#' @importFrom dplyr collect
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr pull
+#' @importFrom purrr map
+#' @importFrom rlang .data
+#' @importFrom tibble tibble
 
 add_locations <- function(locations, type, export = TRUE, db = globaltrends_db) {
-  data <- tibble::tibble(location = locations, type = type)
-  DBI::dbWriteTable(conn = db, name = "data_locations", value = data, append = TRUE)
+  data <- tibble(location = locations, type = type)
+  dbWriteTable(conn = db, name = "data_locations", value = data, append = TRUE)
   
   if (export) .export_locations()
 }
 
+#' @title Export locations to .GlobalEnv
+#'
+#' @keywords internal
+#' @noRd
 
 .export_locations <- function() {
-  locations <- dplyr::distinct(.tbl_locations, type)
+  locations <- distinct(.tbl_locations, type)
   locations <- collect(locations)
   locations <- locations$type
   
