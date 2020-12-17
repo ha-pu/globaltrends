52eb8520681a6385cb25c0f5af0847c5c36204cc
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-12-17 09:25:09 +0100

    Add check and roxygen

--- R/add_locations.r
+++ R/add_locations.r
@@ -1,16 +1,33 @@
 #' @title Add sets of locations
 #'
 #' @description
+#' The function adds a new set of locations for downloads and computations to
+#' the database. The location set serves as input for all download and
+#' computation functions.
 #'
 #' @details
+#' Location sets control the loations for which data is downloaded or to which
+#' computations are applied. By adding new location sets, the default sets
+#' \emph{countries} and \emph{us_states} can be expanded by additional sets.
+#' Thereby, users can compute DOI within a region (e.g. adding EU countries as
+#' a set) or single countries (e.g. adding regions of France as a set). Download
+#' and computation functions check whether data for a location already exists.
+#' Therefore, data will not be duplicated when location data already exists from
+#' another set.
 #'
-#' @param locations xxx.
-#' @param type xxx.
-#' @param export xxx.
+#' @param locations Locations that should be added as set of locations. Vector of
+#' class \code{character}.
+#' @param type Name of the location set that should be added. Object of class
+#' \code{character} of length 1.
+#' @param export Indicator whether the new location set should be directly
+#' exported to .GlobalEnv. Object of class \code{logical}, defaults to
+#' \code{TRUE}.
 #' @param db Connection to database file that should be closed. Defaults
 #' to \code{globaltrends_db}.
 #'
 #' @return
+#' Message that the location set has been created successfully. Location data is
+#' written to table \emph{data_locations}.
 #' 
 #' @example
 #' \dontrun{
@@ -22,7 +39,9 @@
 #' @importFrom dplyr distinct
 #' @importFrom dplyr filter
 #' @importFrom dplyr pull
+#' @importFrom glue glue
 #' @importFrom purrr map
+#' @importFrom purrr walk
 #' @importFrom rlang .data
 #' @importFrom tibble tibble
 
@@ -33,10 +52,20 @@ add_locations <- function(locations, type, export = TRUE, db = globaltrends_db) 
   if (length(export) > 1) stop(glue("Error: 'export' must be object of length 1.\nYou provided an object of length {length(export)}."))
   if (!is.logical(export)) stop(glue("Error: 'export' must be object of type logical.\nYou provided an object of type {typeof(export)}."))
   
+  # check new locations
+  codes <- c(gtrendsR::countries$country_code, gtrendsR::countries$sub_code)
+  codes <- unique(codes)
+  codes <- na.omit(codes)
+  walk(locations, ~{
+    if (!(.x %in% codes)) stop(glue("Error: Invalid input for new location!\nLocation must be part of columns 'country_code' or 'sub_code' of table gtrendsR::countries.\nYou supplied {.x}."))
+  })
+  
   data <- tibble(location = locations, type = type)
   dbWriteTable(conn = db, name = "data_locations", value = data, append = TRUE)
   
   if (export) .export_locations()
+  
+  message(glue("Successfully created new location set {type} ({locations_collapse}).", locations_collapse = paste(locations, collapse = ", ")))
 }
 
 #' @title Export locations to .GlobalEnv
