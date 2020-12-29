7d8d6a9d662ea13370eff1a9be8fbcaaaa42a5a9
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-12-29 17:10:22 +0100

    Check compute_doi

--- R/compute_doi.r
+++ R/compute_doi.r
@@ -72,9 +72,9 @@ compute_doi <- function(object, control = 1, locations = "countries") UseMethod(
 #' @export
 
 compute_doi.numeric <- function(object, control = 1, locations = "countries") {
-  if (length(control) > 1) stop(glue("Error: 'control' must be object of length 1.\nYou provided an object of length {length(control)}."))
-  if (length(locations) != 1) stop(glue("Error: Length object 'locations' must not exeed 1.\nYou provided an object with length {length(locations)}."))
-  if (!is.character(locations)) stop(glue("Error: 'locations' must be object of type character.\nYou provided an object of type {typeof(locations)}."))
+  .check_length(control, 1)
+  .check_length(locations, 1)
+  .check_input(locations, "character")
   if (length(object) > 1) {
     compute_doi(control = control, object = as.list(object), locations = locations)
   } else {
