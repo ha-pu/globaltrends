22242fef40cbef1574238dd9c8e3b8747dd31d40
ha-pu <66355167+ha-pu@users.noreply.github.com>
2021-03-23 22:19:10 +0100

    Update tests

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -18,88 +18,121 @@ dbWriteTable(globaltrends_db, "data_doi", data, append = TRUE)
 
 # export_control ---------------------------------------------------------------
 test_that("export_control1", {
-  out <- export_control(control = 1)
+  out <- export_control()
   expect_equal(nrow(out), 1200)
 })
 
 test_that("export_control2", {
+  out <- export_control(control = 1)
+  expect_equal(nrow(out), 1200)
+})
+
+test_that("export_control3", {
+  out <- export_control(control = NULL)
+  expect_equal(nrow(out), 1200)
+})
+
+test_that("export_control4", {
+  out <- export_control(control = list(1))
+  expect_equal(nrow(out), 1200)
+})
+
+test_that("export_control5", {
   expect_error(
     export_control(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
   )
   expect_error(
     export_control(control = "A"),
-    "Batch number must be object of type integer.\nYou provided a non-integer value."
+    "no applicable method"
   )
   expect_error(
     export_control(control = TRUE),
-    "Batch number must be object of type integer.\nYou provided a non-integer value."
+    "no applicable method"
   )
   expect_error(
     export_control(control = sum),
-    "Batch number must be object of type integer.\nYou provided a non-integer value."
-  )
-  expect_error(
-    export_control(control = 1:5),
-    "'control' must be object of length 1.\nYou provided an object of length 5."
+    "no applicable method"
   )
 })
 
 # export_control_global --------------------------------------------------------
 test_that("export_control_global1", {
-  out <- export_control_global(control = 1)
+  out <- export_control_global()
   expect_equal(nrow(out), 600)
 })
 
 test_that("export_control_global2", {
+  out <- export_control_global(control = 1)
+  expect_equal(nrow(out), 600)
+})
+
+test_that("export_control_global3", {
+  out <- export_control_global(control = NULL)
+  expect_equal(nrow(out), 600)
+})
+
+test_that("export_control_global4", {
+  out <- export_control_global(control = list(1))
+  expect_equal(nrow(out), 600)
+})
+
+test_that("export_control_global5", {
   expect_error(
     export_control_global(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
   )
   expect_error(
     export_control_global(control = "A"),
-    "Batch number must be object of type integer.\nYou provided a non-integer value."
+    "no applicable method"
   )
   expect_error(
     export_control_global(control = TRUE),
-    "Batch number must be object of type integer.\nYou provided a non-integer value."
+    "no applicable method"
   )
   expect_error(
     export_control_global(control = sum),
-    "Batch number must be object of type integer.\nYou provided a non-integer value."
-  )
-  expect_error(
-    export_control_global(control = 1:5),
-    "'control' must be object of length 1.\nYou provided an object of length 5."
+    "no applicable method"
   )
 })
 
 # export_object ----------------------------------------------------------------
 test_that("export_object1", {
+  out <- export_object()
+  expect_equal(nrow(out), 3120)
+})
+
+test_that("export_object2", {
   out <- export_object(keyword = "manchester united")
   expect_equal(nrow(out), 240)
 })
 
-test_that("export_object2", {
+test_that("export_object3", {
+  out <- export_object(keyword = NULL)
+  expect_equal(nrow(out), 3120)
+})
+
+test_that("export_object4", {
+  out <- export_object(keyword = list(c("manchester united", "real madrid")))
+  expect_equal(nrow(out), 480)
+})
+
+test_that("export_object5", {
   expect_error(
     export_object(keyword = 1),
-    "'keyword' must be object of type character.\nYou provided an object of type double."
+    "no applicable method"
   )
   expect_error(
     export_object(keyword = TRUE),
-    "'keyword' must be object of type character.\nYou provided an object of type logical."
+    "no applicable method"
   )
   expect_error(
     export_object(keyword = sum),
-    "'keyword' must be object of type character.\nYou provided an object of type builtin."
-  )
-  expect_error(
-    export_object(keyword = letters[1:5]),
-    "'keyword' must be object of length 1.\nYou provided an object of length 5."
+    "no applicable method"
   )
 })
 
-test_that("export_object3", {
+test_that("export_object6", {
   expect_error(
     export_object(object = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -122,7 +155,7 @@ test_that("export_object3", {
   )
 })
 
-test_that("export_object4", {
+test_that("export_object7", {
   expect_error(
     export_object(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -147,30 +180,41 @@ test_that("export_object4", {
 
 # export_object_global ---------------------------------------------------------
 test_that("export_object_global1", {
+  out <- export_object_global()
+  expect_equal(nrow(out), 1560)
+})
+
+test_that("export_object_global2", {
   out <- export_object_global(keyword = "manchester united")
   expect_equal(nrow(out), 120)
 })
 
-test_that("export_object_global2", {
+test_that("export_object_global3", {
+  out <- export_object_global(keyword = NULL)
+  expect_equal(nrow(out), 1560)
+})
+
+test_that("export_object_global4", {
+  out <- export_object_global(keyword = list(c("manchester united", "real madrid")))
+  expect_equal(nrow(out), 240)
+})
+
+test_that("export_object_global5", {
   expect_error(
     export_object_global(keyword = 1),
-    "'keyword' must be object of type character.\nYou provided an object of type double."
+    "no applicable method"
   )
   expect_error(
     export_object_global(keyword = TRUE),
-    "'keyword' must be object of type character.\nYou provided an object of type logical."
+    "no applicable method"
   )
   expect_error(
     export_object_global(keyword = sum),
-    "'keyword' must be object of type character.\nYou provided an object of type builtin."
-  )
-  expect_error(
-    export_object_global(keyword = letters[1:5]),
-    "'keyword' must be object of length 1.\nYou provided an object of length 5."
+    "no applicable method"
   )
 })
 
-test_that("export_object_global3", {
+test_that("export_object_global6", {
   expect_error(
     export_object_global(object = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -193,7 +237,7 @@ test_that("export_object_global3", {
   )
 })
 
-test_that("export_object_global4", {
+test_that("export_object_global7", {
   expect_error(
     export_object_global(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -218,31 +262,45 @@ test_that("export_object_global4", {
 
 # export_score -----------------------------------------------------------------
 test_that("export_score1", {
-  out <- export_score(keyword = "real madrid")
-  expect_equal(nrow(out), 240)
+  out <- export_score()
+  expect_equal(nrow(out), 1920)
   expect_s3_class(out, "exp_score")
 })
 
 test_that("export_score2", {
-  expect_error(
-    export_score(keyword = 1),
-    "'keyword' must be object of type character.\nYou provided an object of type double."
-  )
-  expect_error(
-    export_score(keyword = TRUE),
-    "'keyword' must be object of type character.\nYou provided an object of type logical."
-  )
-  expect_error(
-    export_score(keyword = sum),
-    "'keyword' must be object of type character.\nYou provided an object of type builtin."
-  )
-  expect_error(
-    export_score(keyword = letters[1:5]),
-    "'keyword' must be object of length 1.\nYou provided an object of length 5."
-  )
+  out <- export_score(keyword = "manchester united")
+  expect_equal(nrow(out), 240)
+  expect_s3_class(out, "exp_score")
 })
 
 test_that("export_score3", {
+  out <- export_score(keyword = NULL)
+  expect_equal(nrow(out), 1920)
+  expect_s3_class(out, "exp_score")
+})
+
+test_that("export_score4", {
+  out <- export_score(keyword = list(c("manchester united", "real madrid")))
+  expect_equal(nrow(out), 480)
+  expect_s3_class(out, "exp_score")
+})
+
+test_that("export_score5", {
+  expect_error(
+    export_score(keyword = 1),
+    "no applicable method"
+  )
+  expect_error(
+    export_score(keyword = TRUE),
+    "no applicable method"
+  )
+  expect_error(
+    export_score(keyword = sum),
+    "no applicable method"
+  )
+})
+
+test_that("export_score6", {
   expect_error(
     export_score(object = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -265,7 +323,7 @@ test_that("export_score3", {
   )
 })
 
-test_that("export_score4", {
+test_that("export_score7", {
   expect_error(
     export_score(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -290,31 +348,45 @@ test_that("export_score4", {
 
 # export_voi -------------------------------------------------------------------
 test_that("export_voi1", {
-  out <- export_voi(keyword = "real madrid")
-  expect_equal(nrow(out), 120)
+  out <- export_voi()
+  expect_equal(nrow(out), 1200)
   expect_s3_class(out, "exp_voi")
 })
 
 test_that("export_voi2", {
-  expect_error(
-    export_voi(keyword = 1),
-    "'keyword' must be object of type character.\nYou provided an object of type double."
-  )
-  expect_error(
-    export_voi(keyword = TRUE),
-    "'keyword' must be object of type character.\nYou provided an object of type logical."
-  )
-  expect_error(
-    export_voi(keyword = sum),
-    "'keyword' must be object of type character.\nYou provided an object of type builtin."
-  )
-  expect_error(
-    export_voi(keyword = letters[1:5]),
-    "'keyword' must be object of length 1.\nYou provided an object of length 5."
-  )
+  out <- export_voi(keyword = "manchester united")
+  expect_equal(nrow(out), 120)
+  expect_s3_class(out, "exp_voi")
 })
 
 test_that("export_voi3", {
+  out <- export_voi(keyword = NULL)
+  expect_equal(nrow(out), 1200)
+  expect_s3_class(out, "exp_voi")
+})
+
+test_that("export_voi4", {
+  out <- export_voi(keyword = list(c("manchester united", "real madrid")))
+  expect_equal(nrow(out), 240)
+  expect_s3_class(out, "exp_voi")
+})
+
+test_that("export_voi5", {
+  expect_error(
+    export_voi(keyword = 1),
+    "no applicable method"
+  )
+  expect_error(
+    export_voi(keyword = TRUE),
+    "no applicable method"
+  )
+  expect_error(
+    export_voi(keyword = sum),
+    "no applicable method"
+  )
+})
+
+test_that("export_voi6", {
   expect_error(
     export_voi(object = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -337,7 +409,7 @@ test_that("export_voi3", {
   )
 })
 
-test_that("export_voi4", {
+test_that("export_voi7", {
   expect_error(
     export_voi(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -362,31 +434,45 @@ test_that("export_voi4", {
 
 # export_doi -------------------------------------------------------------------
 test_that("export_doi1", {
-  out <- export_doi(control = 1, object = 1, type = "trd", locations = "countries")
-  expect_equal(nrow(out), 480)
+  out <- export_doi()
+  expect_equal(nrow(out), 3600)
   expect_s3_class(out, "exp_doi")
 })
 
 test_that("export_doi2", {
-  expect_error(
-    export_doi(keyword = 1),
-    "'keyword' must be object of type character.\nYou provided an object of type double."
-  )
-  expect_error(
-    export_doi(keyword = TRUE),
-    "'keyword' must be object of type character.\nYou provided an object of type logical."
-  )
-  expect_error(
-    export_doi(keyword = sum),
-    "'keyword' must be object of type character.\nYou provided an object of type builtin."
-  )
-  expect_error(
-    export_doi(keyword = letters[1:5]),
-    "'keyword' must be object of length 1.\nYou provided an object of length 5."
-  )
+  out <- export_doi(keyword = "manchester united")
+  expect_equal(nrow(out), 360)
+  expect_s3_class(out, "exp_doi")
 })
 
 test_that("export_doi3", {
+  out <- export_doi(keyword = NULL)
+  expect_equal(nrow(out), 3600)
+  expect_s3_class(out, "exp_doi")
+})
+
+test_that("export_doi4", {
+  out <- export_doi(keyword = list(c("manchester united", "real madrid")))
+  expect_equal(nrow(out), 720)
+  expect_s3_class(out, "exp_doi")
+})
+
+test_that("export_doi5", {
+  expect_error(
+    export_doi(keyword = 1),
+    "no applicable method"
+  )
+  expect_error(
+    export_doi(keyword = TRUE),
+    "no applicable method"
+  )
+  expect_error(
+    export_doi(keyword = sum),
+    "no applicable method"
+  )
+})
+
+test_that("export_doi6", {
   expect_error(
     export_doi(object = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -409,7 +495,7 @@ test_that("export_doi3", {
   )
 })
 
-test_that("export_doi4", {
+test_that("export_doi7", {
   expect_error(
     export_doi(control = 1.5),
     "Batch number must be object of type integer.\nYou provided a non-integer numeric value."
@@ -432,7 +518,7 @@ test_that("export_doi4", {
   )
 })
 
-test_that("export_doi5", {
+test_that("export_doi8", {
   expect_error(
     export_doi(type = 1),
     "'type' must be object of type character.\nYou provided an object of type double."
@@ -455,7 +541,7 @@ test_that("export_doi5", {
   )
 })
 
-test_that("export_doi6", {
+test_that("export_doi9", {
   expect_error(
     export_doi(locations = 1),
     "'locations' must be object of type character.\nYou provided an object of type double."
