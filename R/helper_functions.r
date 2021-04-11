127ab8274522f5f0daf67acd6e6ae681490262b0
ha-pu <66355167+ha-pu@users.noreply.github.com>
2021-04-11 21:43:50 +0200

    Change error wait time

--- R/helper_functions.r
+++ R/helper_functions.r
@@ -13,8 +13,10 @@
 .get_trend <- function(location, term, time = "all") {
   out <- try(gtrends(keyword = term, geo = location, time = time, onlyInterest = TRUE))
   while (inherits(out, "try-error")) {
-    if (attr(out, "condition")$message == "widget$status_code == 200 is not TRUE") {
-      Sys.sleep(3600)
+    if (str_detect(attr(out, "condition")$message, "^<simpleError in get_widget(comparison_item, category, gprop, hl, cookie_url, tz): widget$status_code == 200")) {
+      Sys.sleep(60)
+	} else if (str_detect(attr(out, "condition")$message, "Status code was not 200. Returned status code:500")){
+	  Sys.sleep(1)
     } else {
       Sys.sleep(60)
     }
