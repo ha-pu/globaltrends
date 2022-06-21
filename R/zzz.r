a2f63d60fba6f9c811be8748b105faeec3bf7492
ha-pu <66355167+ha-pu@users.noreply.github.com>
2022-06-21 18:19:39 +0200

    Export gt.env

--- R/zzz.r
+++ R/zzz.r
@@ -1,12 +1,43 @@
+#' @title globaltrends package environment
+#'
+#' @description
+#' The environment `gt.env` contains all package-related data objects, such as
+#' the handle for the SQLite database file or connections to tables. The object
+#' involve:
+#' \itemize{
+#'   \item globaltrends_db: Handle for the SQLite database file.
+#'   \item tbl_locations: Connection to table that contains the lists of locations saved in the database.
+#'   \item tbl_keywords: Connection to table that contains the lists of keywords saved in the database.
+#'   \item tbl_time: Connection to table that contains the lists of batch times saved in the database.
+#'   \item tbl_synonyms: Connection to table that contains the lists of keyword synonyms saved in the database.
+#'   \item tbl_doi: Connection to table that contains the DOI data saved in the database.
+#'   \item tbl_control: Connection to table that contains data on search volume for control terms saved in the database.
+#'   \item tbl_object: Connection to table that contains data on search volume for object terms saved in the database.
+#'   \item tbl_score: Connection to table that contains data on search scores saved in the database.
+#'   \item keywords_control: Tibble that contains all keywords per control batch.
+#'   \item time_control: Tibble that contains all batch times per control batch.
+#'   \item keywords_object: Tibble that contains all keywords per object batch.
+#'   \item time_object: Tibble that contains all batch times per object batch.
+#'   \item keyword_synonyms: Tibble that contains all keyword/synonym combinations.
+#' }
+#' 
+#' @seealso
+#' * [example_control()]
+#' * [example_object()]
+#' * [example_score()]
+#' * [example_doi()]
+#' 
+#' @export gt.env
+
 gt.env <- new.env(parent = emptyenv())
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   lst_name <- list(
     "globaltrends_db",
     "tbl_locations",
     "tbl_keywords",
     "tbl_time",
     "tbl_synonyms",
-	"tbl_doi",
+	  "tbl_doi",
     "tbl_control",
     "tbl_object",
     "tbl_score",
