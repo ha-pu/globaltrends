3128dd5a071c3240a17bbd225a73c9c0f504c9eb
ha-pu <66355167+ha-pu@users.noreply.github.com>
2025-11-19 11:46:35 +0100

    Fix column contents

--- data-raw/generate_data.r
+++ data-raw/generate_data.r
@@ -133,7 +133,8 @@ example_score <- stat_score %>%
   mutate(
     score = out,
     date = lst_dates,
-    batch_c = 1L
+    batch_c = 1L,
+    synonym = 0L
   ) %>%
   unnest(cols = c(score, date))
 
